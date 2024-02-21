e171869c731ffeff1872a249e721a020e26d557e
varunramesh <varunramesh@localhost>
2024-02-21 14:04:38 -0600

    hbu

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -9,6 +9,17 @@ public class Flag {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		
+		String shapetype = in.next();
+		int redComponent = in.nextInt();
+		int blueComponent = in.nextInt();
+		int greenComponent = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		double parameterOne = in.nextDouble();
+		double parameterTwo = in.nextDouble();
+		double parameterThree = in.nextDouble();
+		double parameterFour = in.nextDouble();
+		
+		StdDraw.rectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
 		
 		
 		
